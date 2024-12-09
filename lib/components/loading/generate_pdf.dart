2fdf10f13d22c01ff0227755058707c21736bf25
Tech2cool <aktarvishwas8520@gmail.com>
2024-12-09 19:27:05 +0530

    sss

---
+++ lib/components/loading/generate_pdf.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LoadingSquare extends StatelessWidget {
+  const LoadingSquare({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.3),
+      child: Center(
+        child: Lottie.asset(
+          width: 200,
+          height: 200,
+          "assets/animations/pdf_anim.json",
+        ),
+      ),
+    );
+  }
+}

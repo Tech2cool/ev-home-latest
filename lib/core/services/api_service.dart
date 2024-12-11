1383f47bc0a828022276f73ff341a7db29d68c04
salunkhe04 <salunkheshreya19@gmail.com>
2024-12-11 14:28:35 +0530

    changes made in the closing manager dashboard

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -32,9 +32,9 @@ const storage = FlutterSecureStorage();
 
 // final dio = Dio();
 
-const baseUrl = "http://192.168.1.168:8082";
+// const baseUrl = "http://192.168.1.168:8082";
 
-// const baseUrl = "https://api.evhomes.tech";
+const baseUrl = "https://api.evhomes.tech";
 
 class ApiService {
   static final ApiService _instance = ApiService._internal();

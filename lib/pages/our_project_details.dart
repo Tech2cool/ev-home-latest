ea23b717082202272b7682602efe22dccb214be8
MayurThorat147 <mayurthorat147@gmail.com>
2024-12-12 19:37:18 +0530

    to be done

--- lib/pages/our_project_details.dart
+++ lib/pages/our_project_details.dart
@@ -1,4 +1,5 @@
 import 'package:ev_homes/components/animated_gradient_bg.dart';
+import 'package:ev_homes/components/lottie/lottie1_page.dart';
 import 'package:ev_homes/components/lottie/lottie_page.dart';
 import 'package:ev_homes/core/models/our_project.dart';
 import 'package:ev_homes/core/providers/setting_provider.dart';
@@ -47,7 +48,7 @@ class PropertyCard extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => const Lottiepage(),
+            builder: (context) => const Lottie1Page(),
           ),
         );
 

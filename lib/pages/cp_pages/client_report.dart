331b477181f18ec020cbd62803cefa188c3ed500
salunkhe04 <salunkheshreya19@gmail.com>
2024-12-17 14:11:25 +0530

    do not merge few changes remaining

--- lib/pages/cp_pages/client_report.dart
+++ lib/pages/cp_pages/client_report.dart
@@ -1,10 +1,19 @@
+import 'dart:async';
+
+import 'package:ev_homes/core/helper/helper.dart';
+import 'package:ev_homes/core/models/channel_partner.dart';
+import 'package:ev_homes/core/models/lead.dart';
+import 'package:ev_homes/core/providers/setting_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class ClientReport extends StatefulWidget {
   final String selectedFilter;
+  final String? id;
 
-  const ClientReport({Key? key, required this.selectedFilter}) : super(key: key);
+  const ClientReport({Key? key, required this.selectedFilter, this.id})
+      : super(key: key);
 
   @override
   State<ClientReport> createState() => _ClientReportState();
@@ -16,47 +25,133 @@ class _ClientReportState extends State<ClientReport> {
   String? selectedDateFilter = 'All';
   DateTime? customStartDate;
   DateTime? customEndDate;
+  bool isLoading = false;
+  bool isFetchingMore = false;
+  List<Lead> leads = [];
+  int currentPage = 1;
+  int totalPages = 1;
+  Timer? _debounce;
+  String? stage;
+  ChannelPartner? selectedChannelPartner;
 
   // Dummy data
-  final List<Map<String, String>> dummyClients = [
-    {
-      'clientName': 'John Doe',
-      'clientPhoneNumber': '1234567890',
-      'startDate': '2023-10-01',
-      'validTillDate': '2023-12-01',
-      'taggingStatus': 'Approved'
-    },
-    {
-      'clientName': 'Jane Smith',
-      'clientPhoneNumber': '0987654321',
-      'startDate': '2023-09-15',
-      'validTillDate': '2023-11-15',
-      'taggingStatus': 'Rejected'
-    },
-    {
-      'clientName': 'Emily Johnson',
-      'clientPhoneNumber': '1122334455',
-      'startDate': '2023-08-20',
-      'validTillDate': '2023-10-20',
-      'taggingStatus': 'In Progress'
-    },
-  ];
+  // final List<Map<String, String>> dummyClients = [
+  //   {
+  //     'clientName': 'John Doe',
+  //     'clientPhoneNumber': '1234567890',
+  //     'startDate': '2023-10-01',
+  //     'validTillDate': '2023-12-01',
+  //     'taggingStatus': 'Approved'
+  //   },
+  //   {
+  //     'clientName': 'Jane Smith',
+  //     'clientPhoneNumber': '0987654321',
+  //     'startDate': '2023-09-15',
+  //     'validTillDate': '2023-11-15',
+  //     'taggingStatus': 'Rejected'
+  //   },
+  //   {
+  //     'clientName': 'Emily Johnson',
+  //     'clientPhoneNumber': '1122334455',
+  //     'startDate': '2023-08-20',
+  //     'validTillDate': '2023-10-20',
+  //     'taggingStatus': 'In Progress'
+  //   },
+  // ];
+
+  Future<void> getLeads({bool resetPage = false}) async {
+    final settingProvider = Provider.of<SettingProvider>(
+      context,
+      listen: false,
+    );
+
+    if (resetPage) {
+      setState(() {
+        currentPage = 1; // Reset to first page
+        leads = [];
+        isLoading = true;
+      });
+    } else {
+      setState(() {
+        isFetchingMore = true; // Show loading more indicator
+      });
+    }
+
+    final visitsResp = await settingProvider.searchLeadChannelPartner(
+      widget.id ?? settingProvider.loggedChannelPartner?.id ?? "",
+      searchQuery,
+      currentPage,
+      10,
+      widget.selectedFilter.toLowerCase() == "Leads"
+          ? null
+          : widget.selectedFilter.toLowerCase(),
+      stage,
+      // selectedChannelPartner?.id,
+    );
+    print("Request Parameters:");
+    print("widget.id: ${widget.id}");
+    print(
+        "loggedChannelPartner?.id: ${settingProvider.loggedChannelPartner?.id}");
+    print("searchQuery: $searchQuery");
+    print("currentPage: $currentPage");
+    print("stage: $stage");
+    print("selectedFilter: ${widget.selectedFilter.toLowerCase()}");
+    // print("selectedChannelPartner?.id: ${selectedChannelPartner?.id}");
+
+    print("API Response:");
+// If visitsResp has a `toJson` method
+
+    // print(visitsResp);
+
+    setState(() {
+      if (resetPage) {
+        leads = visitsResp.data; // Set new leads
+      } else {
+        leads.addAll(visitsResp.data); // Append more leads
+      }
+      totalPages = visitsResp.totalPages; // Update total pages
+      isLoading = false; // Hide loading
+      isFetchingMore = false; // Hide loading more
+    });
+
+    print("Total leads: $leads");
+  }
+
+  Future<void> onRefresh() async {
+    try {
+      final settingProvider = Provider.of<SettingProvider>(
+        context,
+        listen: false,
+      );
+
+      setState(() {
+        isLoading = true;
+      });
+      await settingProvider.getChannelPartners();
+    } catch (e) {
+      //
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+    onRefresh();
+    getLeads(resetPage: true);
     selectedFilter = widget.selectedFilter;
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, String>> filteredClients = dummyClients.where((client) {
-      bool passesStatusFilter = selectedFilter == 'All' || client['taggingStatus'] == selectedFilter;
-      bool passesSearchFilter = client['clientName']!.toLowerCase().contains(searchQuery.toLowerCase()) ||
-          client['clientPhoneNumber']!.contains(searchQuery);
-      bool passesDateFilter = _passesDateFilter(client['startDate']!);
-      return passesStatusFilter && passesSearchFilter && passesDateFilter;
-    }).toList();
+    final filteredClients = leads;
+    final settingProvider = Provider.of<SettingProvider>(context);
+    // final loggedChannel = settingProvider.loggedChannelPartner?.id;
+    // getLeads();
+    // print(filteredClients.length);
 
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 218, 240, 246),
@@ -148,12 +243,16 @@ class _ClientReportState extends State<ClientReport> {
               itemCount: filteredClients.length,
               itemBuilder: (context, index) {
                 final client = filteredClients[index];
+                print(filteredClients.length);
+                getLeads();
+
                 return GestureDetector(
                   onTap: () {
                     // Navigate to CpTaggingDetails
                   },
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
                     child: Card(
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(25),
@@ -168,20 +267,21 @@ class _ClientReportState extends State<ClientReport> {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text(
-                                  client['clientName']!,
+                                  '${client.firstName ?? ""} ${client.lastName ?? ""}',
                                   style: const TextStyle(
                                     fontSize: 18,
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
                                 Container(
-                                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 8, horizontal: 16),
                                   decoration: BoxDecoration(
                                     color: Color(0xFF005254),
                                     borderRadius: BorderRadius.circular(12),
                                   ),
                                   child: Text(
-                                    client['clientPhoneNumber']!,
+                                    (client.phoneNumber?.toString()) ?? "0",
                                     style: const TextStyle(
                                       fontSize: 16,
                                       color: Colors.white,
@@ -196,14 +296,14 @@ class _ClientReportState extends State<ClientReport> {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text(
-                                  client['startDate']!,
+                                  client.startDate?.toIso8601String() ?? "",
                                   style: TextStyle(
                                     fontSize: 14,
                                     color: Colors.grey[700],
                                   ),
                                 ),
                                 Text(
-                                  client['validTillDate']!,
+                                  client.validTill?.toIso8601String() ?? "",
                                   style: TextStyle(
                                     fontSize: 14,
                                     color: Colors.grey[700],
@@ -212,13 +312,15 @@ class _ClientReportState extends State<ClientReport> {
                               ],
                             ),
                             const SizedBox(height: 8),
-                            Text(
-                              'Status: ${client['taggingStatus']}',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: _getStatusColor(client['taggingStatus']!),
-                              ),
-                            ),
+                            // Text(
+                            //   Helper.capitalize(
+                            //     getStatus1(lead),
+                            //   ),
+                            //   style: TextStyle(
+                            //     fontSize: 14,
+                            //     color: _getStatusColor(getStatus1(lead)),
+                            //   ),
+                            // ),
                           ],
                         ),
                       ),
@@ -246,26 +348,38 @@ class _ClientReportState extends State<ClientReport> {
     }
   }
 
+  String getStatus1(Lead lead) {
+    if (lead.stage == "visit") {
+      return "${Helper.capitalize(lead.stage ?? "")} ${Helper.capitalize(lead.visitStatus ?? '')}";
+    } else if (lead.stage == "revisit") {
+      return "${Helper.capitalize(lead.stage ?? "")} ${Helper.capitalize(lead.revisitStatus ?? '')}";
+    } else if (lead.stage == "booking") {
+      return "${Helper.capitalize(lead.stage ?? "")} ${Helper.capitalize(lead.bookingStatus ?? '')}";
+    }
+
+    return "${Helper.capitalize(lead.stage ?? "")} ${Helper.capitalize(lead.visitStatus ?? '')}";
+  }
+
   bool _passesDateFilter(String dateString) {
     if (selectedDateFilter == 'All') return true;
-    
+
     DateTime date = DateTime.parse(dateString);
     DateTime now = DateTime.now();
-    
+
     switch (selectedDateFilter) {
       case 'Day':
-        return date.year == customStartDate!.year && 
-               date.month == customStartDate!.month && 
-               date.day == customStartDate!.day;
+        return date.year == customStartDate!.year &&
+            date.month == customStartDate!.month &&
+            date.day == customStartDate!.day;
       case 'Week':
-        return date.isAfter(customStartDate!.subtract(Duration(days: 1))) && 
-               date.isBefore(customEndDate!.add(Duration(days: 1)));
+        return date.isAfter(customStartDate!.subtract(Duration(days: 1))) &&
+            date.isBefore(customEndDate!.add(Duration(days: 1)));
       case 'Month':
-        return date.year == customStartDate!.year && 
-               date.month == customStartDate!.month;
+        return date.year == customStartDate!.year &&
+            date.month == customStartDate!.month;
       case 'Custom':
         return date.isAfter(customStartDate!.subtract(Duration(days: 1))) &&
-               date.isBefore(customEndDate!.add(Duration(days: 1)));
+            date.isBefore(customEndDate!.add(Duration(days: 1)));
       default:
         return true;
     }
@@ -306,7 +420,8 @@ class _ClientReportState extends State<ClientReport> {
         );
         if (picked != null) {
           setState(() {
-            customStartDate = picked.subtract(Duration(days: picked.weekday - 1));
+            customStartDate =
+                picked.subtract(Duration(days: picked.weekday - 1));
             customEndDate = customStartDate!.add(Duration(days: 6));
           });
         }
@@ -341,4 +456,3 @@ class _ClientReportState extends State<ClientReport> {
     }
   }
 }
-
